394e074b589f9b53d165d14ff238861a4661fb40
Ozzy Tasci <oguzcantasci@gmail.com>
2022-11-24 00:40:56 -0500

    Fixed an error in the function

--- eqArrays.js
+++ eqArrays.js
@@ -8,15 +8,19 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let eqStatus;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      eqStatus = true;
-      // console.log("esit");
-    } else {
-      // console.log("olmadi");
-      eqStatus = false;
-      break;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        eqStatus = true;
+        // console.log("esit");
+      } else {
+        // console.log("olmadi");
+        eqStatus = false;
+        break;
+      }
     }
+  } else {
+    eqStatus = false;
   }
   return eqStatus;
 };
