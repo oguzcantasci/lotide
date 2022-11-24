2108c18adf7db20786e88d29e940d6962d28765a
Ozzy Tasci <oguzcantasci@gmail.com>
2022-11-23 19:43:47 -0500

    Implemented the findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+const findKeyByValue = function(obj, value) {
+  let result;
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    if (obj[key] === value) {
+      result = key;
+    }
+  }
+  console.log(result);
+  return result;
+};
