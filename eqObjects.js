f731e5d7fb8ea0c28806bca00699578d4c10409d
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-09 07:18:16 -0500

    Refactored the code fixed some bugs

--- eqObjects.js
+++ eqObjects.js
@@ -3,28 +3,26 @@ const eqArrays = require('./eqArrays');
 const eqObjects = function(obj1, obj2) {
   const keyNames1 = Object.keys(obj1);
   const keyNames2 = Object.keys(obj2);
-  let lengthMatch;
   let result;
 
-  if (keyNames1.length === keyNames2.length) {
-    lengthMatch = true;
-  } else {
-    result = false;
+  if (keyNames1.length !== keyNames2.length) {
+    return false;
   }
 
-  if (lengthMatch) {
-    for (const key1 of keyNames1) {
-      if (Array.isArray(obj1[key1])) {
-        result = eqArrays(obj1[key1], obj2[key1]);
-      } else if (obj1[key1] === obj2[key1]) {
-        result = true;
-      } else {
-        result = false;
-        break;
+  for (const key1 of keyNames1) {
+    if (Array.isArray(obj1[key1])) {
+      result = eqArrays(obj1[key1], obj2[key1]);
+      if (!result) {
+        return result;
       }
+    } else if (obj1[key1] === obj2[key1]) {
+      result = true;
+    } else {
+      return false;
     }
   }
   return result;
 };
+
 
 module.exports = eqObjects;
