3d2e6595611d2b36b5dc31ec46cd19804946c255
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-09 03:00:10 -0500

    Implemented mocha and chai in the test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), undefined);
+//TEST CODE
+describe("The head() function", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  
+  it("returns the only element in a single element array", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("also works with an array of strings", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
