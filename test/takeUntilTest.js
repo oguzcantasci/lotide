660b40e4178234a72ac14329bab1ad90daf1a20d
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-15 13:16:04 -0500

    Add new test for an edge case

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -3,6 +3,7 @@ const takeUntil = require('../takeUntil');
 
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const array2 = ["All", "I", "ever", "have", ",", "redemption", "songs", "to", "Redwood"];
+const array3 = [1, 2, 3, 4, 5, 6,];
 
 describe("#takeUntil", () => {
   it("should return [1, 2, 3, 4, 5, 6] for x >= 7.", () => {
@@ -10,5 +11,8 @@ describe("#takeUntil", () => {
   });
   it("should return ['All', 'I', 'ever', 'have'] for x === ',').", () => {
     assert.deepEqual(takeUntil(array2, x => x === ','), ['All', 'I', 'ever', 'have']);
+  });
+  it("should return the same array if the callback function is never true", () => {
+    assert.deepEqual(takeUntil(array3, x => x >= 7), array3);
   });
 });
