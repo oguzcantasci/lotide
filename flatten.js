e59baece480a9cf018ef0f63c946db115019b681
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-15 14:38:38 -0500

    Add comments for better readability

--- flatten.js
+++ flatten.js
@@ -1,6 +1,6 @@
 const flatten = function(arr) {
-  let flattened = [];
-  for (let i = 0; i < arr.length; i++) {
+  let flattened = []; //declared an empty array to store the flattened arrays
+  for (let i = 0; i < arr.length; i++) { // looping throught the array to see if there are nested arrays inside
     if (Array.isArray(arr[i])) {
       let cbArray = flatten(arr[i]);
       flattened = flattened.concat(cbArray);
--- without.js
+++ without.js
@@ -1,6 +1,8 @@
 const without = function(source, itemsToRemove) {
   let removalStatus;
   let result = [];
+
+  // loop through the array and push the items that return false for the removal to the result array
   for (let i = 0; i < source.length; i++) {
     removalStatus = false;
     for (let item of itemsToRemove) {
