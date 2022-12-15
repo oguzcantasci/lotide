493181bdca9ce7a7089158da4bfa10ad1771d120
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-15 12:52:28 -0500

    Refactor the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,9 @@
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
-  
-
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else  if (!eqArrays(actual, expected)) {
+  } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
   }
 };
